8e276efb520b44eaa3e593dd4d2b2be4c0053d38
srikkar <srikar.vangala@gmail.com>
2020-11-05 00:33:46 +1300

    refined logic and added test cases

--- parcelParser.py
+++ parcelParser.py
@@ -2,43 +2,69 @@ import os
 import yaml
 
 CONFIGURATION_FILE = "conf.yaml"
-THRESHOLD_WEIGHT_NAME = "thresholdWeight"
 
 class ParcelParser:
+    """ Parcel Analyzer Utility """    
     def __init__(self):
-        """Loads configuration yaml file"""
+        """ Loads Configuration file when Object is initialized
+        """
         try:
             configurationFilePath = os.path.join(os.path.dirname(os.path.realpath(__file__)),CONFIGURATION_FILE)
             with open(configurationFilePath, 'r') as configFile:
                 self.configuration = yaml.safe_load(configFile)
-                self.thresholdWeight = self.configuration.get(THRESHOLD_WEIGHT_NAME)
+                self.thresholdWeight = self.configuration.get('weightLimit')
+                self.packageCostUnit = self.configuration.get('packageCostUnit')
+                self.packageDimensionUnit = self.configuration.get('packageDimensionUnit')
         except FileNotFoundError:
             print(CONFIGURATION_FILE+ "is missing .....!")
         except Exception as error:
             print(error)
 
     def checkWeight(self, packageWeight):
-        """ Checking Weight Compatability """
-        if packageWeight > self.thresholdWeight:
-            print('weight exceeded threshold!')
-            return False
-        else:
-            print('weight validation completed!')
-            return True
-    
-    def checkDimensions(self,packageDimensions):
-        """ Checking Dimensions Compatability w.r.to packageTypes available"""
-        response = dict.fromkeys(['packageType','cost'])
-        for packageType in self.configuration.get('packages'):
-            dimensionCounter = 0
-            for key in list(packageDimensions.keys()):
-                if list(packageType.values())[0][key] >= packageDimensions[key]:
-                    dimensionCounter+=1
-                    if dimensionCounter == len(list(packageDimensions.keys())):
-                        #return {'packageType': list(packageType.keys())[0], 'cost':list(packageType.values())[0]['cost']}
-                        response['packageType']= list(packageType.keys())[0]
-                        response['cost']= list(packageType.values())[0]['cost']
-                        return response
-        return response
-        
-    
+        """Validates/Checks input packageWeight with configurred Threshold Value
+        Args:
+            packageWeight (float): A decimal value
+        Returns:
+            Boolean: True, if Weight is below Threshold
+        """        
+        try:
+            if packageWeight > self.thresholdWeight.get('value'):
+                print('weight exceeded limit i.e., '+ str(self.thresholdWeight.get('value'))+' '+ self.thresholdWeight.get('unit'))
+                return False
+            else:
+                print('weight validation completed!')
+                return True
+        except TypeError:
+            raise TypeError('Numeric Input expected!')   
+        except Exception as error:
+            print(error)    
+            
+
+
+    def checkDimensions(self,packageDimensions):        
+        """User package dimensions are iterated against packages Available to suggest the best package
+        Args:
+            packageDimensions (dict): package dimensions as a Dictionary Object
+        Returns:
+            dict: package Type and Cost
+        """
+        try:
+            if not all(isinstance(x, (int,float)) for x in list(packageDimensions.values())):
+                print('Non-Standard input detected!')
+                raise TypeError('Non-Standard Input package detected...!')
+
+            response = dict.fromkeys(['packageType','cost'])
+            for package in sorted(self.configuration.get('packages',[]), key = lambda i: i['length']):
+                dimensionCounter = 0; dimensionsList = list(packageDimensions.keys())
+                for key in dimensionsList:
+                    if package[key] >= packageDimensions[key]:
+                        dimensionCounter+=1
+                        if dimensionCounter == len(dimensionsList):
+                            response['packageType']= package.get('type')
+                            response['cost']= package.get('cost')
+                            return response
+            return response
+        except TypeError:
+            raise TypeError('Numeric Input expected!')   
+        except Exception as error:
+            print(error)   
---
+++ test/test_ParseTheParcel.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import unittest
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../'))
+from parcelParser import ParcelParser
+
+class packageSolutionTests(unittest.TestCase):
+    
+    def test_checkWeightWithinLimits_True(self):
+        
+        subject = ParcelParser()
+        self.assertEqual(subject.checkWeight(20),True)
+    
+
+    def test_checkWeightWithinLimits_False(self):
+        
+        subject = ParcelParser()
+        self.assertEqual(subject.checkWeight(29),False)
+
+    
+    def test_checkWeight_BoundaryValue_True(self):
+        
+        subject = ParcelParser()
+        self.assertEqual(subject.checkWeight(25),True)
+    
+    def test_checkWeight_NonStandardInputs_TypeError(self):
+        
+        subject = ParcelParser()
+        with self.assertRaises(TypeError):
+            subject.checkWeight('hello')
+    
+    def test_checkDimensions_NonStandardInputs_TypeError(self):
+        
+        subject = ParcelParser()
+        userInput = {'length': 110, 'breadth': '300', 'height': 150}
+        with self.assertRaises(TypeError):
+            subject.checkDimensions(userInput)
+    
+    def test_checkDimensions_StandardInputs_dictObject(self):
+        
+        subject = ParcelParser()
+        userInput = {'length': 110, 'breadth': 300, 'height': 150}
+        self.assertIsInstance(subject.checkDimensions(userInput),dict)
+    
+    def test_checkDimensions_Inputs_small(self):
+        
+        subject = ParcelParser()
+        userInput = {'length': 110, 'breadth': 300, 'height': 150}
+        self.assertEqual(subject.checkDimensions(userInput)['packageType'],'Small')
+
+    def test_checkDimensions_Inputs_Medium(self):
+        
+        subject = ParcelParser()
+        userInput = {'length': 300, 'breadth': 300, 'height': 150}
+        self.assertEqual(subject.checkDimensions(userInput)['packageType'],'Medium')
+
+    def test_checkDimensions_Inputs_Large(self):
+        
+        subject = ParcelParser()
+        userInput = {'length': 100, 'breadth': 500, 'height': 150}
+        self.assertEqual(subject.checkDimensions(userInput)['packageType'],'Large')
+
+
+            
+        
+    
+        
