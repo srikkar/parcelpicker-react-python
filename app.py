4f685dbf6bc70329643517b7b7d4a9758e069e46
srikkar <srikar.vangala@gmail.com>
2020-11-05 14:53:07 +1300

    added Logger

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-from flask import Flask,jsonify, make_response
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, make_response
 from flask.globals import request
 from flask_jsonschema_validator import JSONSchemaValidator
 import json
 import os
 import jsonschema
+import logging
+import logging.config
 
+LOG_FILE_NAME = 'logging.conf'
+logging.config.fileConfig(os.path.join(os.path.dirname(os.path.realpath(__file__)), "conf", LOG_FILE_NAME))
+
+# create logger
+logger = logging.getLogger('PackageSolutionService')
 
 app = Flask(__name__)
-JSONSchemaValidator( app = app, root = "schemas" )
+JSONSchemaValidator(app=app, root="schemas")
 
 try:
     from utils import parcelParser
-    
+
 except Exception as error:
     print(error)
+
 
 @app.route('/parseTheParcel/checkAlive', methods=['GET'])
 def checkAlive():
     try:
-        packageSolutionObj = parcelParser.ParcelParser()
         return make_response(jsonify(status='live'), 200)
     except Exception as error:
         print(error)
@@ -28,19 +36,22 @@ def checkAlive():
 @app.route('/parseTheParcel/weightLimit', methods=['GET'])
 def weightLimit():
     try:
-        packageSolutionObj = parcelParser.ParcelParser()
-        return app.response_class(response=json.dumps({'weightLimit': packageSolutionObj.thresholdWeight.get('value'), 'unit': packageSolutionObj.thresholdWeight.get('unit')}),
-                            status=200,
-                            mimetype='application/json')
+        packageSolutionObj = parcelParser.ParcelParser(logger=logger)
+        return app.response_class(
+            response=json.dumps(
+                {'weightLimit': packageSolutionObj.thresholdWeight.get('value'),
+                 'unit': packageSolutionObj.thresholdWeight.get('unit')}),
+            status=200, mimetype='application/json')
     except Exception as error:
         print(error)
         return make_response(jsonify(error=str(error)), 500)
 
+
 @app.route('/parseTheParcel/checkWeight', methods=['POST'])
-@app.validate( 'packageSolution', 'weight' )
+@app.validate('packageSolution', 'weight')
 def checkWeight():
     try:
-        packageSolutionObj = parcelParser.ParcelParser()
+        packageSolutionObj = parcelParser.ParcelParser(logger=logger)
         if packageSolutionObj.checkWeight(request.json['weight']):
             return make_response(jsonify(message='Allowed'), 200)
         else:
@@ -51,33 +62,39 @@ def checkWeight():
 
 
 @app.route('/parseTheParcel/packageSolution', methods=['POST'])
-@app.validate( 'packageSolution', 'dimensions' )
+@app.validate('packageSolution', 'dimensions')
 def packageSolution():
     try:
-        packageSolutionObj = parcelParser.ParcelParser()
+        packageSolutionObj = parcelParser.ParcelParser(logger=logger)
         inputJson = request.json
-        userInput = dict.fromkeys(['length','breadth', 'height'])
+        userInput = dict.fromkeys(['length', 'breadth', 'height'])
         for key in userInput:
             userInput[key] = inputJson[key]
         if checkWeight().json['message'] == "Allowed":
             print("userInput: "+str(userInput))
             return app.response_class(response=json.dumps(packageSolutionObj.checkDimensions(userInput)),
-                            status=200,
-                            mimetype='application/json')
+                                      status=200,
+                                      mimetype='application/json')
         else:
-            return make_response(jsonify(message='weight exceeded limit i.e., '+ str(packageSolutionObj.thresholdWeight.get('value'))+' '+ packageSolutionObj.thresholdWeight.get('unit')), 200)
-    
+            return make_response(
+                jsonify(
+                    message='weight exceeded limit i.e., ' + str(packageSolutionObj.thresholdWeight.get('value')) + ' ' +
+                    packageSolutionObj.thresholdWeight.get('unit')),
+                200)
+
     except Exception as error:
         print(error)
         return make_response(jsonify(error=str(error)), 500)
 
-@app.errorhandler( jsonschema.ValidationError )
-def onValidationError( error ):
-  return make_response(jsonify(error=str(error.message)), 400)
 
-if __name__=='__main__':
+@app.errorhandler(jsonschema.ValidationError)
+def onValidationError(error):
+    return make_response(jsonify(error=str(error.message)), 400)
+
+
+if __name__ == '__main__':
     print("Starting package Solution Service...")
-    if os.getenv('ENV','DEV') == "DEV":
+    if os.getenv('ENV', 'DEV') == "DEV":
         from flask_cors import CORS
         CORS(app)
         app.run(host="127.0.0.1", port="8001", debug=True)
--- utils/parcelParser.py
+++ utils/parcelParser.py
@@ -1,26 +1,30 @@
+# -*- coding: utf-8 -*-
 import os
 import yaml
 
-CONFIGURATION_FILE = "configuration.yaml"
-
 class ParcelParser:
-    """ Parcel Analyzer Utility """    
-    def __init__(self, configurationFilePath=os.path.join(os.path.dirname(os.path.realpath(__file__)),"../","conf", CONFIGURATION_FILE)):
+    """ Parcel Analyzer Utility """
+
+    def __init__(self, logger, configurationFilePath=os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "../", "conf", "configuration.yaml")):
         """ Loads Configuration file when Object is initialized
         """
         try:
             ENV = os.getenv('ENV', 'DEV')
+            self.logger = logger
             with open(configurationFilePath, 'r') as configFile:
                 self.configuration = yaml.safe_load(configFile)
-                self.envConfig = self.configuration[ENV]
-                self.thresholdWeight = self.envConfig.get('weightLimit')
-                self.packageCostUnit = self.envConfig.get('packageCostUnit')
-                self.packageDimensionUnit = self.envConfig.get('packageDimensionUnit')
-                self.errors = self.envConfig.get('ERROR_RESPONSES')
+            self.envConfig = self.configuration[ENV]
+            
+            self.thresholdWeight = self.envConfig.get('weightLimit')
+            self.packageCostUnit = self.envConfig.get('packageCostUnit')
+            self.packageDimensionUnit = self.envConfig.get('packageDimensionUnit')
+            self.errors = self.envConfig.get('ERROR_RESPONSES')
         except FileNotFoundError:
-            print(CONFIGURATION_FILE+ "is missing .....!")
+            self.logger.critical("{} is missing .....!".format(configurationFilePath ))
         except Exception as error:
-            print(error)
+            self.logger.error(error)
 
     def checkWeight(self, packageWeight):
         """Validates/Checks input packageWeight with configurred Threshold Value
@@ -28,22 +32,22 @@ class ParcelParser:
             packageWeight (float): A decimal value
         Returns:
             Boolean: True, if Weight is below Threshold
-        """        
+        """
         try:
-            if packageWeight > self.thresholdWeight.get('value'):
-                print('weight exceeded limit i.e., '+ str(self.thresholdWeight.get('value'))+' '+ self.thresholdWeight.get('unit'))
-                return False
-            else:
-                print('weight validation completed!')
+            if packageWeight <= self.thresholdWeight.get('value') and packageWeight > 0:
+                self.logger.debug('weight validation completed!')
                 return True
+            else:
+                self.logger.warning('weight is not in the expected range 0 to ' +
+                                    str(self.thresholdWeight.get('value')) + ' ' + self.thresholdWeight.get('unit'))
+                return False
+
         except TypeError:
-            raise TypeError(self.errors['typeError']) 
+            raise TypeError(self.errors['typeError'])
         except Exception as error:
-            print(error)    
-            
+            self.logger.error(error)
 
-
-    def checkDimensions(self,packageDimensions):        
+    def checkDimensions(self, packageDimensions):
         """User package dimensions are iterated against packages Available to suggest the best package
         Args:
             packageDimensions (dict): package dimensions as a Dictionary Object
@@ -51,24 +55,25 @@ class ParcelParser:
             dict: package Type and Cost
         """
         try:
-            if not all(isinstance(x, (int,float)) for x in list(packageDimensions.values())):
-                print('Non-Standard input detected!')
-                raise TypeError('Non-Standard Input package detected...!')
+            if not all(isinstance(x, (int, float)) for x in list(packageDimensions.values())):
+                self.logger.warning('Non-Numeric input detected!')
+                raise TypeError('Non-Numeric Input package detected...!')
 
-            outputObject = dict.fromkeys(['packageType','cost'])
-            for package in sorted(self.envConfig.get('packages',[]), key = lambda i: i['length']):
-                dimensionCounter = 0; dimensionsList = list(packageDimensions.keys())
+            outputObject = dict.fromkeys(['packageType', 'cost'])
+            for package in sorted(self.envConfig.get('packages', []), key=lambda i: i['length']):
+                dimensionCounter = 0
+                dimensionsList = list(packageDimensions.keys())
                 for key in dimensionsList:
-                    if package[key] >= packageDimensions[key]:
-                        dimensionCounter+=1
+                    if package[key] >= packageDimensions[key] and packageDimensions[key] > 0:
+                        dimensionCounter += 1
                         if dimensionCounter == len(dimensionsList):
-                            outputObject['packageType']= package.get('type')
-                            outputObject['cost']= """{}{}""".format(self.packageCostUnit, package.get('cost'))
+                            outputObject['packageType'] = package.get('type')
+                            outputObject['cost'] = """{}{}""".format(self.packageCostUnit, package.get('cost'))
                             return outputObject
-            
+
             outputObject['exception'] = self.errors['packageNotFound']
             return outputObject
         except TypeError:
-            raise TypeError(self.errors['typeError'])   
+            raise TypeError(self.errors['typeError'])
         except Exception as error:
-            print(error)   
+            self.logger.error(error)
